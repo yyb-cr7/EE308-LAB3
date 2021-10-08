978cab97267327312d2c1f217c63896709f3b07b
yyb-cr7 <332842829@qq.com>
2021-10-09 01:35:44 +0800

    third

--- miniprogram/pages/index/main_index.js
+++ miniprogram/pages/index/main_index.js
@@ -7,70 +7,22 @@ Page({
     data: {
         msg : "规则"
     },
+    
+        toenterRoom(){
+            wx.navigateTo({
+              url: '/pages/enterRoom/enterRoom',
+            })
+          },
+          tosingleGame(){
+            wx.navigateTo({
+              url: '/pages/singleGame/singleGame',
+            })
+          },
+          tocreateRoom(){
+            wx.navigateTo({
+              url: '/pages/createRoom/createRoom',
+            })
+          }
 
-    /**
-     * 生命周期函数--监听页面加载
-     */
-    onLoad: function (options) {
-        console.log('onload()')
-        //修改状态数据
-        console.log(this.data.log) // 代表当前界面的实例对象
-        
-        this.setData({
-            msg : '单机游戏'
-        })
-        console.log(this.data.log) // 代表当前界面的实例对象)
-        setTimeout(()=>{
-
-        },10)
-    },
-
-    /**
-     * 生命周期函数--监听页面初次渲染完成
-     */
-    onReady: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面显示
-     */
-    onShow: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面隐藏
-     */
-    onHide: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面卸载
-     */
-    onUnload: function () {
-
-    },
-
-    /**
-     * 页面相关事件处理函数--监听用户下拉动作
-     */
-    onPullDownRefresh: function () {
-
-    },
-
-    /**
-     * 页面上拉触底事件的处理函数
-     */
-    onReachBottom: function () {
-
-    },
-
-    /**
-     * 用户点击右上角分享
-     */
-    onShareAppMessage: function () {
-
-    }
+    
 })
